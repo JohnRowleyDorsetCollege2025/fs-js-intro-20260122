7263a1dd14e9d2cf9e58c7b02bbe6204ebd5502d
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2026-01-22 15:17:12 +0000

    update

--- GroupA/loadapi/js/site.js
+++ GroupA/loadapi/js/site.js
@@ -11,13 +11,36 @@ async function loadCourses() {
     const data = await response.json();
     console.log(data);
     renderCourses(data);
+    renderCoursesAsTable(data)
 }
 
 function renderCourses(courses) {
     var container = document.getElementById("courses");
+
     courses.forEach(function (course) {
         const p = document.createElement("p");
         p.innerHTML=course.courseName;
         container.append(p);
     })   
+}
+
+function renderCoursesAsTable(courses) {
+
+    var tbody = document.querySelector("#courseTable tbody");
+    
+    courses.forEach(function (course) {
+        const row = document.createElement("tr");
+
+        cellId = document.createElement("td");
+        cellId.textContent=course.id;
+        row.append(cellId);
+        
+        cellTitle = document.createElement("td");
+        cellTitle.textContent=course.courseName;
+        row.append(cellTitle);
+       
+        tbody.append(row);
+    })   
+
+   
 }
